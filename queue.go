6e8380970d7641cc8f650f4e04972b17cc8c9f98
Jared Ellison <jaredellison@gmail.com>
2023-02-27 23:11:05 -0500

    feat: implement queue.Run

--- queue.go
+++ queue.go
@@ -1,28 +1,127 @@
 package main
 
 import (
+	"fmt"
+	"sync"
 	"time"
 )
 
 type Queue struct {
-	tasks    []task
-	Interval time.Duration
-	Retries  int
+	tasks      []task
+	tasksLock  sync.Mutex
+	Interval   time.Duration
+	Retries    int
+	status     map[string]bool
+	statusLock sync.Mutex
 }
 
 type task struct {
-	Run func() bool
-	Try int
+	Id   string
+	Run  func() error
+	Try  int
+	Done bool
 }
 
-func (q *Queue) AddTasks(ts []func() bool) {
-	for _, t := range ts {
-		q.tasks = append(q.tasks, task{
-			Run: t,
-		})
+func NewQueue(ts []func() error, interval time.Duration, retries int) *Queue {
+	q := Queue{
+		Interval: interval,
+		status:   map[string]bool{},
+		Retries:  retries,
 	}
+	for i, t := range ts {
+		tsk := task{
+			Run: t,
+			Id:  fmt.Sprintf("%d", i), // Use UUID here?
+		}
+		q.tasks = append(q.tasks, tsk)
+		q.status[tsk.Id] = false
+	}
+	return &q
+}
+
+func (q *Queue) GetTask() *task {
+	q.tasksLock.Lock()
+	defer q.tasksLock.Unlock()
+	if len(q.tasks) == 0 {
+		return nil
+	}
+	tsk := q.tasks[0]
+	q.tasks = q.tasks[1:]
+	return &tsk
+}
+
+func (q *Queue) RetryTask(t *task) {
+	q.tasksLock.Lock()
+	defer q.tasksLock.Unlock()
+	q.tasks = append(q.tasks, *t)
+}
+
+func (q *Queue) MarkDone(id string) {
+	q.statusLock.Lock()
+	defer q.statusLock.Unlock()
+	q.status[id] = true
+}
+
+func (q *Queue) CheckDone() bool {
+	q.statusLock.Lock()
+	defer q.statusLock.Unlock()
+	for _, done := range q.status {
+		if !done {
+			return false
+		}
+	}
+	return true
 }
 
 func (q *Queue) Run() {
+	// Start Wait Group
+	wg := sync.WaitGroup{}
 
+	// Start ticker
+	ticker := time.NewTicker(q.Interval)
+	done := make(chan struct{})
+
+	// Start processing tasks
+	wg.Add(1)
+	go func() {
+		for {
+			select {
+			case <-done:
+				wg.Done()
+				return
+			case <-ticker.C:
+				// Pull task from queue
+				tsk := q.GetTask()
+				if tsk == nil {
+					continue
+				}
+				// Increment try count
+				tsk.Try++
+				// Try to run it in a go routine
+				go func() {
+					err := tsk.Run()
+					// Log error
+					if err != nil {
+						fmt.Printf("Error handling task %s: %v\n", tsk.Id, err)
+					}
+
+					// If success or reached retry limit we're done
+					if err == nil || tsk.Try >= q.Retries {
+						// Mark done in map
+						q.MarkDone(tsk.Id)
+						// Check if other tasks are done
+						if q.CheckDone() {
+							done <- struct{}{}
+						}
+						return
+					}
+
+					// Try again
+					q.RetryTask(tsk)
+				}()
+			}
+		}
+	}()
+
+	wg.Wait()
 }
--- queue_test.go
+++ queue_test.go
@@ -1,22 +1,22 @@
 package main
 
 import (
+	"errors"
 	"testing"
 	"time"
 )
 
 func TestQueue(t *testing.T) {
-	t.Run("Queue.AddTasks adds tasks to the queue to be run", func(t *testing.T) {
-		q := Queue{
-			Interval: time.Millisecond,
-			Retries:  0,
-		}
-
-		q.AddTasks([]func() bool{
-			func() bool { return true },
-			func() bool { return false },
-			func() bool { return true },
-		})
+	t.Run("NewQueue adds tasks to the queue to be run", func(t *testing.T) {
+		q := NewQueue(
+			[]func() error{
+				func() error { return nil },
+				func() error { return nil },
+				func() error { return nil },
+			},
+			time.Millisecond,
+			3,
+		)
 
 		taskCount := len(q.tasks)
 		if taskCount != 3 {
@@ -27,6 +27,36 @@ func TestQueue(t *testing.T) {
 			if tsk.Try != 0 {
 				t.Errorf("expect tasks to start on try 0, got %d", tsk.Try)
 			}
+		}
+	})
+
+	t.Run("Queue.Run should retry funcs multiple times", func(t *testing.T) {
+		runCountA := 0
+		runCountB := 0
+
+		q := NewQueue(
+			[]func() error{
+				func() error {
+					runCountA += 1
+					return errors.New("example error")
+				},
+				func() error {
+					runCountB += 1
+					return errors.New("example error")
+				},
+			},
+			time.Millisecond,
+			3,
+		)
+
+		q.Run()
+
+		if runCountA != 3 {
+			t.Errorf("want 3 retries, got %d", runCountA)
+		}
+
+		if runCountB != 3 {
+			t.Errorf("want 3 retries, got %d", runCountB)
 		}
 	})
 }
